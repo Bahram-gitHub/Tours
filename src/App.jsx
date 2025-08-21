0daa370de5250a720a64b784f2ecd3f5bdad3b4a
bta62 <bta62@sfu.ca>
2025-08-21 16:33:13 -0700

    loading Tour and tours added and they are displaying in the window

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,39 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Loading from './Loading';
+import Tours from './Tours'
+
 const url = 'https://www.course-api.com/react-tours-project';
 
-const App = () => {
-  return <h2>Tours Starter</h2>;
-};
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if(isLoading){
+    return <main>
+      <Loading>
+
+      </Loading>
+    </main>
+  }
+  return <main>
+    <Tours tours={tours}></Tours>
+  </main>;
+}
 export default App;
